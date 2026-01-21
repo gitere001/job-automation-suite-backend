e101edb7c978a4703f899b1f354cadd9b47af932
James gitere <gitere.dev@gmail.com>
2026-01-21 12:21:17 +0300

    updated

--- routes/jobApplicationRoutes.js
+++ routes/jobApplicationRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-import { sendBatchApplications } from '../controllers/jobApplicationController.js';
+import { sendBatchApplications, getDashboardStats } from '../controllers/jobApplicationController.js';
 
 const router = Router();
 
 router.post('/send-applications', sendBatchApplications);
+router.get('/dashboard-stats', getDashboardStats);
 
 
 export default router;
